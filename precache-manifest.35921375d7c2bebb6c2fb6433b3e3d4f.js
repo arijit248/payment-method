c08e5a74e513b42defdb0d28071a9648ac75d87a
Arjun <Arijit Kundu>
2019-11-07 15:03:57 +0530

    Updates

---
+++ precache-manifest.35921375d7c2bebb6c2fb6433b3e3d4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5181ea8e6005d0cc9759630e40b09d51",
+    "url": "/arijit248/payment-method.git/index.html"
+  },
+  {
+    "revision": "6262c3a2d4350a1d4b32",
+    "url": "/arijit248/payment-method.git/static/css/main.93d57a44.chunk.css"
+  },
+  {
+    "revision": "b977e32802f31061c0b6",
+    "url": "/arijit248/payment-method.git/static/js/2.61cbfc03.chunk.js"
+  },
+  {
+    "revision": "6262c3a2d4350a1d4b32",
+    "url": "/arijit248/payment-method.git/static/js/main.6251f43e.chunk.js"
+  },
+  {
+    "revision": "f3e317ed183f607ccf83",
+    "url": "/arijit248/payment-method.git/static/js/runtime-main.4b75f65c.js"
+  }
+]);
